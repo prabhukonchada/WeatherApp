8d85cf6e5276caa8faa54cacb1dc4b30712c4f6c
prabhukonchada <prabhukonchada@gmail.com>
2017-09-23 17:36:16 +0530

    Added a new menu to hold share menu item. Used sharecompat to share data

--- app/src/main/java/weatherapp/prabhukonchada/android/weatherapp/DetailActivity.java
+++ app/src/main/java/weatherapp/prabhukonchada/android/weatherapp/DetailActivity.java
@@ -1,10 +1,16 @@
 package weatherapp.prabhukonchada.android.weatherapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -25,4 +31,23 @@ public class DetailActivity extends AppCompatActivity {
         weatherInfoItem.setText(data);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.detail_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        String mimeType = "text/plain";
+        String title = "Weather Data";
+
+        if(item.getItemId() == R.id.shareMenu)
+        {
+            Log.d("Share","Share Clicked");
+            Intent shareIntent = ShareCompat.IntentBuilder.from(this).setType(mimeType).setChooserTitle(title).setText(data).getIntent();
+            startActivity(shareIntent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
